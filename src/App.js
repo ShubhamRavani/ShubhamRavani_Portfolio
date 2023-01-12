57a1e46bf9fe1d1dc22f1dcfe7993ca787fdaf7f
Shubham Ravani <shubhamravani67890@gmail.com>
2023-01-13 01:04:27 +0530

    ShubhamRavani

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Analytics } from "@vercel/analytics/react";
 import { About, Footer, Header, Skills, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
@@ -12,6 +12,7 @@ const App = () => (
     <Work />
     <Skills />
     <Footer />
+    <Analytics />
   </div>
 );
 
